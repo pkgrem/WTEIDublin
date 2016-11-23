def7c2f0836d225ad6bb49bb969090e69293b560
Brian Motherway <brian@limelighthealth.com>
2016-11-23 19:06:07 +0000

    Added google places API

--- businesses.php
+++ businesses.php
@@ -4,50 +4,37 @@
 
 	$businesses = [];
 
-	/* 
-
-	**************************************
-	UNCOMMENT ME WHEN QUERY LIMIT IS RESET
-
-	// foreach ($group_reference_ids as $food_group => $reference_ids) {
-	// 	foreach ($reference_ids as $index => $reference_id) {
-	//		//assigns reference id to grab business data for
-	// 		$google_places->reference = $reference_id;
-	//		//business data
-	// 		$business_details = $google_places->details();
-	// 		foreach ($business_details as $details) {
-	// 			if (isset($business_details['result'])) {
-	//				//store only the data we want in an array and categorises it under it's respective food group
-	// 				$businesses[$food_group][$details['id']] = array (
-	// 					'name' => $details['name'],
-	// 					'open_now' => $details['opening_hours']['open_now'],
-	// 					'opening_hours' => $details['opening_hours']['weekday_text'],
-	// 					'rating' => $details['rating'],
-	// 					'map_url' => $details['url'],
-	// 					'website' => $details['website']
-	// 				);
-	// 			}
-	// 		}
-	// 	}
-	// }
-	
-	**************************************
-	*/
-
-	/*
-	**************************************
-	COMMENT ME OUT WHEN QUERY LIMIT IS RESET
-	*/
-	foreach ($groups as $group) {
-		$businesses[$group][] = array (
-			'name' => 'name',
-			'open_now' => 1,
-			'opening_hours' => 'hours',
-			'rating' => 4.2,
-			'map_url' => 'url',
-			'website' => 'https://www.google.ie'
-		);
+	//queries the google places api for business data if we delete our local data in businesses.json
+	if (!file_exists('businesses.json') || filesize('businesses.json') == 0) {
+		foreach ($group_reference_ids as $food_group => $reference_ids) {
+			foreach ($reference_ids as $index => $reference_id) {
+				//assigns reference id to grab business data for
+				$google_places->reference = $reference_id;
+				//business data
+				$business_details = $google_places->details();
+				echo '<pre>' . print_r($business_details, true) . '</pre>';
+				if (isset($business_details['result']['website'])) {
+					//store only the data we want in an array and categorises it under it's respective food group
+					$businesses[$food_group][$business_details['result']['id']] = array (
+						'name' => isset($business_details['result']['name']) ? $business_details['result']['name'] : '',
+						'open_now' => isset($business_details['result']['opening_hours']['open_now']) ? $business_details['result']['opening_hours']['open_now'] : '',
+						'opening_hours' => isset($business_details['result']['opening_hours']['weekday_text']) ? $business_details['result']['opening_hours']['weekday_text'] : '',
+						'rating' => isset($business_details['result']['rating']) ? $business_details['result']['rating'] : '',
+						'map_url' => isset($business_details['result']['url']) ? $business_details['result']['url'] : '',
+						'address' => isset($business_details['result']['formatted_address']) ? $business_details['result']['formatted_address'] : '',
+						'phone' => isset($business_details['result']['formatted_phone_number']) ? $business_details['result']['formatted_phone_number'] : '',
+						'website' => isset($business_details['result']['website']) ? $business_details['result']['website'] : ''
+					);
+				}
+			}
+		}
+		$fp = fopen('businesses.json', 'w');
+		fwrite($fp, json_encode($businesses));
+		fclose($fp);
+	//queries a local file that already contains business data
+	//This is to increase performance and limit how often we ping google places api
+	} else {
+		$businesses = json_decode(file_get_contents('businesses.json'), true);
 	}
-	//*************************************
 
 ?>
--- index.php
+++ index.php
@@ -26,12 +26,77 @@ input[type=text]:focus {
     width: 100%;
 }
 
+form.search_box {
+  margin-bottom: -5%;
+}
+
 html, body { height: 100%; padding: 0; margin: 0; }
 div { width: 25%; height: 25%; float: left; margin-top: 5% }
-#div1 { background: #DDD; }
-#div2 { background: #AAA; }
-#div3 { background: #777; }
-#div4 { background: #444; }
+
+/* The Overlay (background) */
+.overlay {
+    /* Height & width depends on how you want to reveal the overlay (see JS below) */    
+    height: 100%;
+    width: 0;
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    background-color: rgb(0,0,0); /* Black fallback color */
+    background-color: rgba(0,0,0, 0.9); /* Black w/opacity */
+    overflow-x: hidden; /* Disable horizontal scroll */
+    transition: 0.5s; /* 0.5 second transition effect to slide in or slide down the overlay (height or width, depending on reveal) */
+}
+
+/* Position the content inside the overlay */
+.overlay-content {
+    top: 25%; /* 25% from the top */
+    width: 100%; /* 100% width */
+    text-align: center; /* Centered text/links */
+    margin-top: 30px; /* 30px top margin to avoid conflict with the close button on smaller screens */
+}
+
+.overlay-content h1{
+    color: white;
+}
+
+/* The navigation links inside the overlay */
+.overlay a {
+    padding: 8px;
+    text-decoration: none;
+    font-size: 36px;
+    color: #818181;
+    display: block; /* Display block instead of inline */
+    transition: 0.3s; /* Transition effects on hover (color) */
+}
+
+/* When you mouse over the navigation links, change their color */
+.overlay a:hover, .overlay a:focus {
+    color: #f1f1f1;
+}
+
+/* Position the close button (top right corner) */
+.overlay .closebtn {
+    position: absolute;
+    top: 20px;
+    right: 45px;
+    font-size: 60px;
+}
+
+img{ 
+  cursor: pointer; 
+  cursor: hand; 
+}
+
+/* When the height of the screen is less than 450 pixels, change the font-size of the links and position the close button again, so they don't overlap */
+@media screen and (max-height: 450px) {
+    .overlay a {font-size: 20px}
+    .overlay .closebtn {
+        font-size: 40px;
+        top: 15px;
+        right: 35px;
+    }
+}
 
 </style>
 
@@ -39,28 +104,52 @@ div { width: 25%; height: 25%; float: left; margin-top: 5% }
 <body>
 
 <p>What Should I Eat in Dublin?</p>
-<form>
+<form class="search_box">
   <input type="text" name="search" placeholder="Search for a kind of food..">
 </form>
 
 </br></br>
 <?php $i = 1 ?>
 <?php foreach ($groups as $food_type): ?>
-  <?php // UNCOMMENT ME WHEN QUERY LIMIT IS RESET $random_business = array_rand($businesses[$food_type], 1); ?>
-  <div id="div<?php print $i ?>">
-  <!-- UNCOMMENT ME WHEN QUERY LIMIT IS RESET 
-    <a href="<?php //print $random_business['website'] ?>"> 
-  -->
-  <!-- COMMENT ME OUT WHEN QUERY LIMIT IS RESET -->
-    <a href="<?php print $businesses[$food_type][0]['website'] ?>">
-  <!-- **************** -->
-      <img src="http://www.paddykeoghgoode.com/whatshouldieatindublin/<?php print $food_type ?>.png">
-    </a>
+  <?php $random_business_id = array_rand($businesses[$food_type], 1); ?>
+  <?php // echo '<pre>' . print_r($businesses[$food_type][$random_business_id], true) . '</pre>' ?>
+  <div id="div<?php print $i ?>"
+    data-website="<?php print $businesses[$food_type][$random_business_id]['website'] ?>"
+    data-name="<?php print $businesses[$food_type][$random_business_id]['name'] ?>"
+  >
+      <img src="http://www.paddykeoghgoode.com/whatshouldieatindublin/<?php print $food_type ?>.png" onclick="openNav(<?php print($i) ?>)">
   </div>
-  <?php $i++ ?>
+<?php $i++ ?>
 <?php endforeach; ?>
 
+<div id="myNav" class="overlay">
+<!-- Button to close the overlay navigation -->
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <!-- Overlay content -->
+  <div class="overlay-content">
+    <h1 id="company-name">Company Name</h1>
+    <a id="website-link" href="#">URL</a>
+  </div>
+</div>
 
+<script type="text/javascript">
+  /* Open when someone clicks on the span element */
+function openNav(counter) {
+    document.getElementById("myNav").style.width = "100%";
+
+    var id = "div" + counter;
+    var website = document.getElementById(id).getAttribute("data-website");
+    var name = document.getElementById(id).getAttribute("data-name");
+    document.getElementById("website-link").setAttribute("href",website);
+    document.getElementById("website-link").innerHTML = website;
+    document.getElementById("company-name").innerHTML = name;
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+</script>
 
 </body>
 </html>
